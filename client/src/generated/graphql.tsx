76c6de7b7faf933963c664d291f588fb8a2c5676
NguyenDangKhuong <nguyendangkhuong96@gmail.com>
2021-09-23 15:54:32 +0700

    list product

--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -171,6 +171,11 @@ export type MeQueryVariables = Exact<{ [key: string]: never; }>;
 
 export type MeQuery = { __typename?: 'Query', me?: Maybe<{ __typename?: 'User', id: string, username: string, email: string }> };
 
+export type ProductsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type ProductsQuery = { __typename?: 'Query', products?: Maybe<Array<{ __typename?: 'Product', id: string, title: string, description: string, price: number, createdAt: any, updatedAt: any }>> };
+
 export const ProductMutationStatusesFragmentDoc = gql`
     fragment productMutationStatuses on ProductMutationResponse {
   code
@@ -361,3 +366,42 @@ export function useMeLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<MeQuery
 export type MeQueryHookResult = ReturnType<typeof useMeQuery>;
 export type MeLazyQueryHookResult = ReturnType<typeof useMeLazyQuery>;
 export type MeQueryResult = Apollo.QueryResult<MeQuery, MeQueryVariables>;
+export const ProductsDocument = gql`
+    query Products {
+  products {
+    id
+    title
+    description
+    price
+    createdAt
+    updatedAt
+  }
+}
+    `;
+
+/**
+ * __useProductsQuery__
+ *
+ * To run a query within a React component, call `useProductsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useProductsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useProductsQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useProductsQuery(baseOptions?: Apollo.QueryHookOptions<ProductsQuery, ProductsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<ProductsQuery, ProductsQueryVariables>(ProductsDocument, options);
+      }
+export function useProductsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<ProductsQuery, ProductsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<ProductsQuery, ProductsQueryVariables>(ProductsDocument, options);
+        }
+export type ProductsQueryHookResult = ReturnType<typeof useProductsQuery>;
+export type ProductsLazyQueryHookResult = ReturnType<typeof useProductsLazyQuery>;
+export type ProductsQueryResult = Apollo.QueryResult<ProductsQuery, ProductsQueryVariables>;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,43 @@
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import NavBar from '../components/NavBar'
+import { ProductsDocument, useProductsQuery } from '../generated/graphql'
+import { addApolloState, initializeApollo } from '../lib/apolloClient'
 
-const Index = () => (
-  <>
-    <NavBar />
-    <h1></h1>
-  </>
-)
+export const limit = 3
+
+const Index = () => {
+  const { data, loading } = useProductsQuery()
+  return (
+    <>
+      <NavBar />
+      {loading ? (
+        '...LOADING'
+      ) : (
+        <ul>
+          {data?.products?.map((product) => (
+            <li>{product.title}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const apolloClient = initializeApollo({ headers: context.req.headers })
+
+  await apolloClient.query({
+    query: ProductsDocument,
+    variables: {
+      limit
+    }
+  })
+
+  return addApolloState(apolloClient, {
+    props: {}
+  })
+}
 
 export default Index
